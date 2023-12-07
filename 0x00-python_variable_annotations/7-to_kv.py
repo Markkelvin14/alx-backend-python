dc09f622eaae3c27d4945c83f11d6ea33d9e2385
Markkelvin14 <mjkelvin38@gmail.com>
2023-12-07 12:12:48 +0000

    variable_annotations

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,4 +8,5 @@ from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    return(k, v**2)
+    '''A function that retuns a tuple'''
+    return (k, v**2)
