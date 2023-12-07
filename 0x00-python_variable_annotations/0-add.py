860ced3b60ffd29c71134e8904f362043695b05b
Markkelvin14 <mjkelvin38@gmail.com>
2023-12-07 10:59:33 +0000

    variable_annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+'''A python module that returns the sum of two float numbers'''
+
+
+def add(a: float, b: float) -> float:
+    '''A function that returns the sum of two float numbers'''
+    return a + b
